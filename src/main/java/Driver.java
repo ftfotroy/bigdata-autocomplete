d4e0cfd1b2065cae60d64b6cbf704ab555161a29
Zhang Yi <zhangyi0704@gmail.com>
2017-03-13 22:28:52 -0700

    Initial commit

---
+++ src/main/java/Driver.java
@@ -0,0 +1,53 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.db.DBConfiguration;
+import org.apache.hadoop.mapreduce.lib.db.DBOutputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import java.io.IOException;
+
+
+public class Driver {
+
+	public static void main(String[] args) throws ClassNotFoundException, IOException, InterruptedException {
+		//job1
+		Configuration configuration1 = new Configuration();
+
+		//customize separator
+		configuration1.set("textinputformat.record.delimiter", ".");
+		configuration1.set("noGram", args[2]);
+
+		Job job1 = Job.getInstance();
+		job1.setJobName("NGram");
+		job1.setJarByClass(Driver.class);
+
+		job1.setMapperClass(NGramLibraryBuilder.NGramMapper.class);
+		job1.setReducerClass(NGramLibraryBuilder.NGramReducer.class);
+
+		job1.setOutputKeyClass(Text.class);
+		job1.setOutputValueClass(IntWritable.class);
+
+		job1.setInputFormatClass(TextInputFormat.class);
+		job1.setOutputFormatClass(TextOutputFormat.class);
+
+		TextInputFormat.setInputPaths(job1, new Path(args[0]));
+		TextOutputFormat.setOutputPath(job1, new Path(args[1]));
+		job1.waitForCompletion(true);
+
+
+
+
+		
+		//how to connect two jobs?
+		// last output is second input
+		
+		//2nd job
+
+	}
+
+}
---
+++ src/main/java/NGramLibraryBuilder.java
@@ -0,0 +1,70 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Mapper.Context;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+
+
+public class NGramLibraryBuilder {
+	private static Logger logger = Logger.getLogger(NGramLibraryBuilder.class);
+	public static class NGramMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+		int noGram;
+		@Override
+		public void setup(Context context) {
+			//how to get n-gram from command line?
+			Configuration configuration = context.getConfiguration();
+			noGram = configuration.getInt("noGram", 5);
+		}
+
+		// map method
+		@Override
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+			logger.info("NGramLibraryBuilder: NgramMapper: ngram mapper started");
+			String line = value.toString().trim().toLowerCase();
+
+			//remove useless elements
+			line = line.replaceAll("[^a-z]", " ");
+
+			//separate word by space
+			String[] words = line.split("\\s+");
+
+			//build n-gram based on array of words
+			if (words.length < 2) {
+				return;
+			}
+			for (int i = 0; i < words.length; i++) {
+				StringBuilder sb = new StringBuilder();
+				sb.append(words[i]);
+				for (int j = 1; j < noGram && i + j < words.length; j++) {
+					sb.append(" ");
+					sb.append(words[i + j]);
+					String str = sb.toString().trim();
+					logger.debug("NGramLibraryBuilder: NgramMapper: ngram result string: " + str);
+					context.write(new Text(str), new IntWritable(1));
+				}
+			}
+		}
+	}
+
+	public static class NGramReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+		// reduce method
+		@Override
+		public void reduce(Text key, Iterable<IntWritable> values, Context context)
+				throws IOException, InterruptedException {
+
+			//sum up the total count for each n-gram
+			int sum = 0;
+			for (IntWritable value : values) {
+				sum += value.get();
+			}
+			context.write(key, new IntWritable(sum));
+		}
+	}
+
+}
